4fd03816f76c488fcec8667e6b9f7549cbab6598
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2024-01-16 21:26:17 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,7 @@
+//package BASICS;
+class first {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hi Naveen Raghav--09/01/2024");
+    }
+}
