9f7b455780cc9a204c1117f384c1cb504af01003
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2024-02-02 04:41:41 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,24 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class test {
+    public static int main(String[] args) {
+        System.out.println("hello--naveen--");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size of array");
+        int n = sc.nextInt();
+        System.out.println(n);
+        //orderOf(arr[n]);
+    
+
+    int orderOf(int arr[n]){
+        Arrays.sort(arr);
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int temp = arr[i] * (arr.length - i);
+            ans = ans + temp;
+        }
+        return ans;
+    }
+}
+}
